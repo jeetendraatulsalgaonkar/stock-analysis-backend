6273ee754e0542805d04688ada29fd7eb5beff90
Jeetendra Salgaonkar <jeetendra-atul.salgaonkar@sixt.com>
2025-02-10 20:44:37 +0100

    [AB-0000]: Moved some files for better organization.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TickersModuleG } from './tickers/module/tickersModuleG';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { GraphQLModule } from '@nestjs/graphql';
-import { upperCaseDirectiveTransformer } from './stocks/directives/upper-case.directive';
+import { upperCaseDirectiveTransformer } from './tickers/directives/upper-case.directive';
 import { TickersModule } from './stocks/modules/tickers.module';
 import { ConfigModule } from '@nestjs/config';
 
